f80a2a88ad3a3c4eaf859fecd179d4285248ca59
cisu <49948070+cisu@users.noreply.github.com>
2021-02-26 22:06:59 +0200

    finished

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -3,7 +3,7 @@ import articleStyles from '../styles/Article.module.css';
 
 const ArticleList = ({articles}) => {
   return (
-    <div className='articleStyles.grid'>
+    <div className={articleStyles.grid}>
       {articles.map((article) => (
         <ArticleItem article={article} />
       ))}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
-import Head from 'next/head';
 import ArticleList from '../components/ArticleList';
+import {server} from '../config';
 
 export default function Home({articles}) {
   return (
     <div>
-      <Head>
-        <title>WebDev News</title>
-        <meta name='keywords' content='web development, programing' />
-      </Head>
-
       <ArticleList articles={articles} />
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
+  const res = await fetch(`${server}/api/articles`);
 
   const articles = await res.json();
 
@@ -27,3 +20,17 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
